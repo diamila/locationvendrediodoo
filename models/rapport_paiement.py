012651a29485ba7583262f906befbd5c24be4654
root <diamila>
2020-08-06 08:29:58 -0700

    first commit

--- models/rapport_paiement.py
+++ models/rapport_paiement.py
@@ -1,23 +1,65 @@
 from odoo import models, fields, api, tools, exceptions, _
 from datetime import datetime, timedelta
 
-# Vente/Quittance
+# Heritage Vente/Quittance
 class Quittance(models.Model):
     _inherit = 'sale.order'  #
 
-    bien_loue = fields.Many2many('product.template', ondelete='cascade', string="Bien Loué", required=True)
-    bailleur_id = fields.Many2one(related='bien_loue.bailleur_id', ondelete='cascade', string="Bailleur")
+    #bailleur_id = fields.Many2one(related='bien_loue.bailleur_id', ondelete='cascade', string="Bailleur")
 
-    list_price = fields.Float(related='bien_loue.list_price', ondelete='cascade', string="montant")
-    total = fields.Float(related='bien_loue.list_price', ondelete='cascade', string="Total")
 
-    periode_debut = fields.Date(string="Période Payée Début")
-    periode_fin = fields.Date(string="Période Payée Fin")
+    #bien_loue = fields.Many2many('product.template', ondelete='cascade', string="Bien Loué", required=True)
+    #bailleur_id = fields.Many2one(related='bien_loue.bailleur_id', ondelete='cascade', string="Bailleur")
+
+    #list_price = fields.Float(related='bien_loue.list_price', ondelete='cascade', string="montant")
+    #total = fields.Float(related='bien_loue.list_price', ondelete='cascade', string="Total")
+
+    #periode_debut = fields.Date(string="Période Payée Début")
+    #periode_fin = fields.Date(string="Période Payée Fin")
 
 class order_line(models.Model):
     _inherit = 'sale.order.line'
 
-    periode_fin =  fields.Date(string='Date payement',default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    #periode_fin =  fields.Date(string='Date payement',default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    mois_payee_c = fields.Selection([('janvier', 'janvier'),
+                                   ('fevrier', 'février'),
+                                   ('mars', 'mars'),
+                                   ('avril', 'avril'),
+                                   ('mai', 'mai'),
+                                   ('juin', 'juin'),
+                                   ('juillet', 'juillet'),
+                                   ('aout', 'août'),
+                                   ('septembre', 'septembre'),
+                                   ('octobre', 'octobre'),
+                                   ('novembre', 'novembre'),
+                                   ('decembre', 'décembre')],
+                                  string="paiement du mois", required=True)
+
+
+
+#heritage pour la facture
+class facture(models.Model):
+    _inherit = 'account.invoice'
+
+    #mois = fields.Char(string="paiement du mois", required=True)
+    #nom = fields.Char(string="Nom du Bien")
+
+    mois_payee = fields.Selection([('janvier', 'janvier'),
+                                   ('fevrier', 'février'),
+                                    ('mars', 'mars'),
+                                   ('mai', 'mai'),
+                                   ('avril', 'avril'),
+                                   ('mai', 'mai'),
+                                   ('juin', 'juin'),
+                                   ('juillet', 'juillet'),
+                                   ('aout', 'août'),
+                                   ('septembre', 'septembre'),
+                                   ('octobre', 'octobre'),
+                                   ('novembre', 'novembre'),
+                                   ('decembre', 'décembre')],
+                                  string="paiement du mois", required=True)
+
 
 class order(models.Model):
     _inherit = 'account.invoice.line'
